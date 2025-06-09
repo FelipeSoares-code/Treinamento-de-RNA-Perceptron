5fe815fb8421a09a007b466ae415c8495c141de0
Felipe Soares <140653664+FelipeSoares-code@users.noreply.github.com>
2025-06-09 23:31:56 +0000

    logica pronta, falta melhorar o sistema

--- Main.py
+++ Main.py
@@ -1,29 +1,31 @@
 print(
-    "Este é um treinamento de RNA Perceptron!",
-    "Você deverá fornecer 3 grupos de valores X1, X2 e X3.\n" \
-    "Além do valor(Y) esperado de cada grupo de valores e a taxa de aprendizado do sistema",
-    "Com base nesses valores e o resultado de saída esperado, " \
-    "o algoritmo irá determinar o peso(W) ideal para cada entrada(X)",
+    "Bem-vindo ao treinamento do Perceptron!",
+    
+    "Você deverá fornecer os valores numéricos de X1, X2 e X3 para 3 objetos,",
+    "além do valor esperado de saída (Y) para cada um deles e a taxa de aprendizado do sistema.",
+    
+    "Com base nesses dados, o algoritmo irá ajustar os pesos (W) de cada entrada (X),",
+    "buscando encontrar os valores ideais para alcançar os resultados esperados.\n",
     sep="\n\n"
 )
 
 entradas = {
-    'grupo 1': {
-        'X1' : float(input('Grupo 1\nX1: ')),
+    'objeto 1': {
+        'X1' : float(input('objeto 1\nX1: ')),
         'X2' : float(input('X2: ')),
         'X3' : float(input('X3: ')),
         'Y'  : float(input('Valor de saída (Y) esperado (0 ou 1): '))
     },
 
-    'grupo 2': {
-        'X1' : float(input('Grupo 2\nX1: ')),
+    'objeto 2': {
+        'X1' : float(input('objeto 2\nX1: ')),
         'X2' : float(input('X2: ')),
         'X3' : float(input('X3: ')),
         'Y'  : float(input('Valor de saída (Y) esperado (0 ou 1): '))
     },
 
-    'grupo 3': {
-        'X1' : float(input('Grupo 3\nX1: ')),
+    'objeto 3': {
+        'X1' : float(input('objeto 3\nX1: ')),
         'X2' : float(input('X2: ')),
         'X3' : float(input('X3: ')),
         'Y'  : float(input('Valor de saída (Y) esperado (0 ou 1): '))
@@ -31,7 +33,7 @@ entradas = {
 }
 
 bias = None
-match input("Usará Bias?: ").upper():
+match input("Usará Bias? [S/N]: ").upper():
     case 'S':
         bias = 1
     case 'N':
@@ -43,25 +45,41 @@ match input("Usará Bias?: ").upper():
 taxaAprendizagem = float(input('Digite o valor da taxa de aprendizagem: '))
 
 w = [0.2, 0.2, 0.2, 0.2] #pesos dos Xs e o bias
-
 fim = False
+erros = [] #lista para armazenar os erros de cada iteração
+numeroIteracao = 0
 while not fim:
-    erros = []
-    for grupo in entradas.values():
+    numeroIteracao += 1
+    print(f"\n\nIteração {numeroIteracao}")
+    erros.clear() #limpa a lista de erros a cada iteração
+    
+    for i, objeto in enumerate(entradas.values()):
         net = 0
 
-        xValues = [grupo['X1'], grupo['X2'], grupo['X3'], bias]
-        for i, x in enumerate(xValues): #pega cada item de x_values (x) e o índice(i)
-            net += x * w[i]
-            w[i] += taxaAprendizagem * erro * xValues[i] #da novo valor para os pesos
+        xValues = [objeto['X1'], objeto['X2'], objeto['X3'], bias]
+        #if bias:
+        #   xValues.append(bias)
+        for j, x in enumerate(xValues): #pega cada item de x_values (x) e o índice(i)
+            net += x * w[j]
             
-        y = 1 if net >= 0 else 0
-        erro = grupo['Y'] - y
+        y = 1 if net > 0 else 0
+        erro = objeto['Y'] - y
         erros.append(erro)
-        
+        print(f"objeto {i + 1} erro: {erro}")
+
+        for j, x in enumerate(xValues): #pega cada item de x_values (x) e o índice(i)
+            w[j] += taxaAprendizagem * erro * xValues[j] #da novo valor para os pesos
+
+
+    print("Erros: ", erros)
+    print("Pesos novos:", w)
+
     if all(e == 0 for e in erros):
         fim = True
-        print("Treinamento concluído!")
+        print(f"\n\nTreinamento concluído!\nForam realizadas {numeroIteracao} iterações")
+
+print("\n\nPesos finais:", w)
+input("Pressione Enter para sair...")
 
             
 
